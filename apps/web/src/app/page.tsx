2b433f82cfaadf1e23e51893c3e3d611084932e5
lewis-wow <ludvik.prokopec@gmail.com>
2026-06-28 08:53:41 +0000

    fix

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Page } from '@repo/ui/components/Page';
 
 export default function HomePage() {
   return (
-    <Page title="Welcome" description="Your new Next.js app.">
+    <Page>
       <p>Hello world</p>
     </Page>
   );
