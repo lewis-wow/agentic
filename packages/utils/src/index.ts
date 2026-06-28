1451aced2afe688e5378197ce14502cd9a4a79ce
lewis-wow <ludvik.prokopec@gmail.com>
2026-06-28 19:33:39 +0000

    add utils

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,29 @@
+import Schema from 'effect/Schema';
+
+export const emptyStringAsUndefined = <K extends string>(
+  obj: Record<K, string | undefined>,
+): Record<K, string | undefined> => {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [
+      key,
+      value === '' ? undefined : value,
+    ]),
+  ) as Record<K, string | undefined>;
+};
+
+export const createEnv = <Fields extends Schema.Struct.Fields>(options: {
+  schema: Fields;
+  runtimeEnv: Record<string, string | undefined>;
+  emptyStringAsUndefined?: boolean;
+}): Schema.Schema.Type<Schema.Struct<Fields>> => {
+  options.emptyStringAsUndefined = options.emptyStringAsUndefined ?? true;
+
+  if (options.emptyStringAsUndefined) {
+    options.runtimeEnv = emptyStringAsUndefined(options.runtimeEnv);
+  }
+
+  const envSchema = Schema.Struct(options.schema);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return Schema.decodeUnknownSync(envSchema as any)(options.runtimeEnv) as any;
+};
