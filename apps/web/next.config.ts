6e4993a4d15dfc87becdf0bac7d49c144bde4eaa
lewis-wow <ludvik.prokopec@gmail.com>
2026-06-28 18:48:30 +0000

    remove web

--- apps/web/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  transpilePackages: ['@repo/ui'],
-  devIndicators: false,
-};
-
-export default nextConfig;
--- apps/web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Page } from '@repo/ui/components/Page';
-
-export default function HomePage() {
-  return (
-    <Page>
-      <p>Hello world</p>
-    </Page>
-  );
-}
