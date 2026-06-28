fbbc8ef38df08fc4781055518df6b532aea84099
lewis-wow <ludvik.prokopec@gmail.com>
2026-06-28 08:36:19 +0200

    use enum

--- packages/ui/src/components/Stack.tsx
+++ packages/ui/src/components/Stack.tsx
@@ -1,9 +1,15 @@
 import { Slot } from '@radix-ui/react-slot';
 import { cn } from '@repo/ui/lib/utils';
+import type { ValueOfEnum } from '@repo/types';
 import type { ReactNode } from 'react';
 
-export const StackPredefinedGap = ['between', 'around', 'evenly'] as const;
-export type StackPredefinedGap = (typeof StackPredefinedGap)[number];
+export const STACK_PREDEFINED_GAP = {
+  BETWEEN: 'BETWEEN',
+  AROUND: 'AROUND',
+  EVENLY: 'EVENLY',
+} as const;
+
+export type StackPredefinedGap = ValueOfEnum<typeof STACK_PREDEFINED_GAP>;
 
 export type StackProps = {
   direction: 'row' | 'column';
@@ -30,14 +36,14 @@ export const Stack = ({
         {
           'flex-col': direction === 'column',
           'flex-row': direction === 'row',
-          'justify-between': gap === 'between',
-          'justify-around': gap === 'around',
-          'justify-evenly': gap === 'evenly',
+          'justify-between': gap === STACK_PREDEFINED_GAP.BETWEEN,
+          'justify-around': gap === STACK_PREDEFINED_GAP.AROUND,
+          'justify-evenly': gap === STACK_PREDEFINED_GAP.EVENLY,
         },
         className,
       )}
       style={{
-        gap: (StackPredefinedGap as readonly string[]).includes(gap)
+        gap: (Object.values(STACK_PREDEFINED_GAP) as string[]).includes(gap)
           ? undefined
           : gap,
       }}
